9bbdc5a680119b82dc024e2de4364983ddb4cf7c
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-11-09 22:15:59 +0600

    dark-light toggle done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,33 @@
+import { createContext, useState } from "react";
 import Accordian from "../../components/Accordian/Accordian";
 import Banner from "../../components/Banner/Banner";
 import BookCategories from "../../components/BookCategories/BookCategories";
 import Footer from "../../components/Footer/Footer";
 import RegistrationSection from "../../components/RegistrationSection/RegistrationSection";
-
+import ReactSwitch from "react-switch";
+import "./Home.css"
+export const ThemeContext = createContext(null);
 const Home = () => {
+    const [theme, setTheme] = useState("dark");
+    const toggleTheme = () => {
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    };
     return (
-        <div>
-            <Banner></Banner>
-            <BookCategories></BookCategories>
-            <RegistrationSection></RegistrationSection>
-            <Accordian></Accordian>
-            <Footer></Footer>
-        </div>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div id={theme}>
+                <div className="switch text-center p-5 flex flex-row gap-5 justify-center items-center">
+                    <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+                    <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}></ReactSwitch>
+                </div>
+                <Banner></Banner>
+                <BookCategories></BookCategories>
+                <RegistrationSection></RegistrationSection>
+                <Accordian></Accordian>
+                <Footer></Footer>
+
+
+            </div>
+        </ThemeContext.Provider>
     );
 };
 
